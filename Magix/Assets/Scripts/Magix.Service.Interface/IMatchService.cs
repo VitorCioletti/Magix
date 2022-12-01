6a3c6688ab31b8b508cd6c3bfd1880379b671cab
VitorCioletti <ciolettiv@gmail.com>
2022-11-30 21:08:22 -0300

    Added service interface.

---
+++ Magix/Assets/Scripts/Magix.Service.Interface/IMatchService.cs
@@ -0,0 +1,12 @@
+﻿namespace Magix.Service.Interface
+{
+    using Domain.Interface;
+    using Domain.Interface.Board;
+
+    public interface IMatchService
+    {
+        IBoard Board { get; }
+
+        IPlayer StartNew();
+    }
+}
